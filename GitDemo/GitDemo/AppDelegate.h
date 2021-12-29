6ff88f93e120d00396211256d12715da4f8cd6f6
Jack_xu <daquan.xu@poolin.com>
2021-12-29 18:59:43 +0800

    first commit

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by xdq on 2021/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by xdq on 2021/12/29.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
